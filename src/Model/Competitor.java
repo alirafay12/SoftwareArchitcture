40b9b93119e2c2ac7d2bea0c6fcee74d0249fb55
alirafay12 <128826125+alirafay12@users.noreply.github.com>
2024-01-06 23:51:44 +0500

    Commit1

---
+++ src/Model/Competitor.java
@@ -0,0 +1,15 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Competitor {
+    private int competitorNumber;
+    private String name;
+    private String email;
+    private  String country;
+    private String dateOfBirth;
+    private String category;
+    private String level;
+    private int[] scores;

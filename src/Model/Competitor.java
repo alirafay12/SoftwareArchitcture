3ab35b75862a72ac90aee1faedee738b7e2477ff
alirafay12 <128826125+alirafay12@users.noreply.github.com>
2024-01-07 00:11:29 +0500

    Commit10

--- src/Model/Competitor.java
+++ src/Model/Competitor.java
@@ -3,7 +3,6 @@ package Model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 public class Competitor {
     private int competitorNumber;
     private String name;

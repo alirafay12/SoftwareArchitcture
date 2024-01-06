02fa05bdfef74a786eda00772487840a50b0fb05
alirafay12 <128826125+alirafay12@users.noreply.github.com>
2024-01-06 23:54:37 +0500

    Commit2

--- src/Model/Competitor.java
+++ src/Model/Competitor.java
@@ -13,3 +13,15 @@ public class Competitor {
     private String category;
     private String level;
     private int[] scores;
+
+    public Competitor(int competitorNumber, String name, String email,String country, String dateOfBirth, String category, String level, int[] scores) {
+        this.competitorNumber = competitorNumber;
+        this.name = name;
+        this.email = email;
+        this.country=country;
+        this.dateOfBirth = dateOfBirth;
+        this.category = category;
+        this.level = level;
+        this.scores = scores;
+    }
+

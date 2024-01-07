6bfd53920439815881b5ed69cdfd25c03497271d
alirafay12 <128826125+alirafay12@users.noreply.github.com>
2024-01-07 18:01:45 +0500

    Commit15

---
+++ src/service/RegisterService.java
@@ -0,0 +1,35 @@
+package service;
+
+import model.Competitor;
+import model.CompetitorList;
+import util.CompetitorNumberGenerator;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class RegisterService {
+
+    public static String registerCompetitor(Competitor competitor, boolean isUpdate) {
+        if (isAnyFieldOmitted(competitor)) {
+            return "Error: Please fill in all the required fields.";
+        }
+
+        if (!isUpdate && isCompetitorAlreadyRegistered(competitor)) {
+            return "Error: A competitor with the same email and category already exists.";
+        }
+
+        if (!isUpdate && isCompetitorWithEmailAndDifferentCategory(competitor)) {
+            int newCompetitorNumber = CompetitorNumberGenerator.generateNextCompetitorNumber();
+            competitor.setCompetitorNumber(newCompetitorNumber);
+            return "Success: model.Competitor registered with a new competitor number: " + newCompetitorNumber;
+        }
+
+        if (!isAgeIncompatibleWithLevel(competitor)) {
+            return "Error: Age is incompatible with the selected level. Please resubmit the form for a different level.";
+        }
+
+        return "Success: model.Competitor registered successfully.";
+    }
+

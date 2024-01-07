53e1de1c4ff958f378d56d0a8a4ba260e116d088
alirafay12 <128826125+alirafay12@users.noreply.github.com>
2024-01-08 04:18:07 +0500

    Commit42

---
+++ src/view/CompetitorRegistrationApp.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import model.CompetitorList;
+import model.FieldEvent;
+import model.FieldEventCompetitor;
+import model.TrackEventCompetitor;
+import service.RegisterService;
+import util.CompetitorNumberGenerator;
+import util.FileUtility;
+

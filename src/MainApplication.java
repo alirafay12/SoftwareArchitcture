a2f8db8b8ff493177b3fe63e815323ce0b097b64
alirafay12 <128826125+alirafay12@users.noreply.github.com>
2024-01-08 17:46:17 +0500

    Commit47

---
+++ src/MainApplication.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import util.FileUtility;
+import view.CompetitorManagementApp;
+import view.CompetitorRegistrationApp;
+
+public class MainApplication extends Application {
+    private static final String STAFF_PASSWORD = "staff"; // Replace with your actual password
+
